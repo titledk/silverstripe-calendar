a7dc3439de8383178ad85d3334ff5c666a69dce3
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-04-22 22:26:25 +0700

    WIP: Tidying up admin interface

--- javascript/admin/CalendarAdmin.js
+++ javascript/admin/CalendarAdmin.js
@@ -1,51 +1,40 @@
+/*jslint white: true */
+console.log('calendar admin');
+
 (function($) {
-	$(function () {
-				// live binding, applies to any inserted elements as well
+    $(document).ready(function () {
+        $.entwine(function ($) {
+            $('#Form_ItemEditForm_AllDay').entwine({
+                onmatch: function (e) {
+                    updateStatus();
+                    this._super();
+                },
+                onchange: function (e) {
+                    updateStatus();
+                    this._super();
+                }
+            });
+        });Form_ItemEditForm_DurationGroup_Holder
+
+        function updateStatus() {
+            var allDay = $('#Form_ItemEditForm_AllDay').is(":checked");
+            console.log('all day', allDay);
+            if (allDay == true) {
+                console.log('all day is yes');
+                $('#Form_ItemEditForm_Duration_Holder').hide();
 
 
 
+                //  $('#Form_ItemEditForm_DurationGroup_Holder').hide();
+                $('#Form_ItemEditForm_EndDateTime_Holder').click();
+            } else {
+                $('#Form_ItemEditForm_Duration_Holder').show();
+
+                // $('Form_ItemEditForm_DurationGroup_Holder').attr('style', 'display:block;');
+            }
 
 
-		$.entwine('ss', function($){
-
-
-//			/**
-//			 * Initializing event specific fields when event form is loaded
-//			 */
-//			$('.CalendarEventGridfieldDetailForm fieldset').entwine({
-//				onmatch: function() {
-//					var form = $(this).closest('form');
-//
-//					var eventForm = new EventFields(form);
-//					eventForm.init();
-//
-//					//console.log(form);
-//					//console.log('initialized');
-//				}
-//			});
-
-
-
-
-		});
-
-	});
+        }
+    })
 })(jQuery);
 
-
-
-//(function($) {
-//
-//
-////Tab direct links - obsolete, but kept for reference
-//	$.entwine('ss', function($) {
-//		$('.CalendarAdmin .cms-panel-link').entwine({
-//			onclick: function(e) {
-//				$('form').addClass('loading');
-//				location.href = $(this).attr('href');
-//			}
-//		});
-//	});
-//
-//
-//})(jQuery);
