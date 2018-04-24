c0f34a6e28c0f1d13c3ff89fee077985e3731bf6
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-04-25 02:16:17 +0700

    MINOR: A single text

---
+++ tests/Events/EventTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace TitleDK\Calendar\Tests\Events;
+
+
+use SilverStripe\Dev\SapphireTest;
+use TitleDK\Calendar\Events\Event;
+
+class EventTest extends SapphireTest {
+    public function setUp()
+    {
+        /** @var Event event */
+        $this->event = new Event();
+        $this->event->Title = 'Test Event Title';
+        $this->event->Details = 'This is detail about the test event title';
+    }
+
+    public function test_details_summary()
+    {
+        $this->assertEquals('wibble', $this->event->DetailsSummary());
+    }
+}
