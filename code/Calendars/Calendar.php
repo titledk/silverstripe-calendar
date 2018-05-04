8747fb81e79630aec30fe2730839a9db8646dad6
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-05-04 11:37:44 +0700

    WIP: Groups editing

--- code/Calendars/Calendar.php
+++ code/Calendars/Calendar.php
@@ -1,7 +1,9 @@
 <?php
 namespace TitleDK\Calendar\Calendars;
 
+use SilverStripe\Forms\ListboxField;
 use SilverStripe\ORM\DataObject;
+use SilverStripe\Security\Group;
 use SilverStripe\Security\Permission;
 
 /**
@@ -31,14 +33,54 @@ class Calendar extends DataObject
     );
 
 
-    //Public calendars are simpley called 'Calendar'
+    //Public calendars are simply called 'Calendar'
     private static $singular_name = 'Calendar';
     private static $plural_name = 'Calendars';
+
+    // for applying group restrictions
+    private static $belongs_many_many = array(
+        'Groups' => Group::class,
+    );
 
 
     public function getCMSFields()
     {
         $fields = parent::getCMSFields();
+
+        $groupsMap = array();
+        foreach (Group::get() as $group) {
+            // Listboxfield values are escaped, use ASCII char instead of &raquo;
+            $groupsMap[$group->ID] = $group->getBreadcrumbs(' > ');
+        }
+        asort($groupsMap);
+
+        $fields->addFieldToTab(
+            'Root.Main',
+            ListboxField::create('DirectGroups', Group::singleton()->i18n_plural_name())
+                ->setSource($groupsMap)
+                ->setAttribute(
+                    'data-placeholder',
+                    _t(__CLASS__ . '.ADDGROUP', 'Add group', 'Placeholder text for a dropdown')
+                )
+        );
+
+        /**
+         * $groupsMap = array();
+        foreach (Group::get() as $group) {
+        // Listboxfield values are escaped, use ASCII char instead of &raquo;
+        $groupsMap[$group->ID] = $group->getBreadcrumbs(' > ');
+        }
+        asort($groupsMap);
+        $fields->addFieldToTab(
+        'Root.Main',
+        ListboxField::create('DirectGroups', Group::singleton()->i18n_plural_name())
+        ->setSource($groupsMap)
+        ->setAttribute(
+        'data-placeholder',
+        _t(__CLASS__ . '.ADDGROUP', 'Add group', 'Placeholder text for a dropdown')
+        )
+        );
+         */
 
         //Events shouldn't be editable from here by default
         $fields->removeByName('Events');
