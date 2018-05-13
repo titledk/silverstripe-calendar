f716c80afb6c806249de2927354050d6f0ba6f4d
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-05-13 20:35:29 +0700

    ENHANCEMENT: Show image in summary

--- code/Events/EventImageExtension.php
+++ code/Events/EventImageExtension.php
@@ -18,14 +18,21 @@ class EventImageExtension extends DataExtension
         'FeaturedImage' => Image::class
     );
 
+    private static $summary_fields = ['Thumbnail'];
+
     public function updateCMSFields(FieldList $fields)
     {
         $uploadField = new UploadField('FeaturedImage', 'Featured Image');
         $uploadField->setAllowedFileCategories('image');
         $uploadField->setFolderName('events');
         $fields->addFieldToTab('Root.Main', $uploadField);
-
     }
 
-
+    public function getThumbnail() {
+        if ($image = $this->owner->FeaturedImage()) {
+            return $image->Fit(80,80);
+        } else {
+            return '(No Image)';
+        }
+    }
 }
