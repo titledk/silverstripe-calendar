7d37aef1a48cd2ebd5c4a3f9e3731a167260a7e2
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-05-16 14:00:23 +0700

    WIP: Adding tests for embargo

---
+++ tests/Events/EventRegistrationEmbargoTest.php
@@ -0,0 +1,69 @@
+<?php
+namespace TitleDK\Calendar\Tests\Events;
+
+
+use Carbon\Carbon;
+use SilverStripe\Dev\SapphireTest;
+use TitleDK\Calendar\Events\Event;
+
+class EventRegistrationEmbargoTest extends SapphireTest {
+    /** @var Carbon */
+    protected $now;
+
+    /** @var Event */
+    protected $event;
+
+    /*
+     * | Field                    | Type                                     | Null | Key | Default                       | Extra          |
++--------------------------+------------------------------------------+------+-----+-------------------------------+----------------+
+| ID                       | int(11)                                  | NO   | PRI | NULL                          | auto_increment |
+| ClassName                | enum('TitleDK\\Calendar\\Events\\Event') | YES  | MUL | TitleDK\Calendar\Events\Event |                |
+| LastEdited               | datetime                                 | YES  | MUL | NULL                          |                |
+| Created                  | datetime                                 | YES  |     | NULL                          |                |
+| Title                    | varchar(200)                             | YES  |     | NULL                          |                |
+| AllDay                   | tinyint(1) unsigned                      | NO   |     | 0                             |                |
+| NoEnd                    | tinyint(1) unsigned                      | NO   |     | 0                             |                |
+| StartDateTime            | datetime                                 | YES  | MUL | NULL                          |                |
+| TimeFrameType            | enum('Duration','DateTime')              | YES  |     | Duration                      |                |
+| Duration                 | time                                     | YES  |     | NULL                          |                |
+| EndDateTime              | datetime                                 | YES  |     | NULL                          |                |
+| Details                  | mediumtext                               | YES  |     | NULL                          |                |
+| EventPageID              | int(11)                                  | NO   | MUL | 0                             |                |
+| CalendarID               | int(11)                                  | NO   | MUL | 0                             |                |
+| Registerable             | tinyint(1) unsigned                      | NO   |     | 0                             |                |
+| TicketsRequired          | tinyint(1) unsigned                      | NO   |     | 0                             |                |
+| PaymentRequired          | tinyint(1) unsigned                      | NO   |     | 0                             |                |
+| RSVPEmail                | varchar(255)                             | YES  |     | NULL                          |                |
+| CostCurrency             | varchar(3)                               | YES  |     | NULL                          |                |
+| CostAmount               | decimal(19,4)                            | NO   |     | 0.0000                        |                |
+| FeaturedImageID          | int(11)                                  | NO   | MUL | 0                             |                |
+| URLSegment               | varchar(255)                             | YES  |     | NULL                          |                |
+| MemberCostCurrency       | varchar(3)                               | YES  |     | NULL                          |                |
+| MemberCostAmount         | decimal(19,4)                            | NO   |     | 0.0000                        |                |
+| LocationName             | varchar(255)                             | YES  |     | NULL                          |                |
+| MapURL                   | varchar(255)                             | YES  |     | NULL                          |                |
+| NumberOfAvailableTickets | int(11)                                  | NO   |     | 0                             |                |
+| RegistrationEmbargoAt    | datetime                                 | YES  |     | NULL
+     */
+
+    public function setUp()
+    {
+        $this->now = Carbon::create(2018, 5, 16, 8);
+        Carbon::setTestNow($$this->now);
+
+        /** @var Event event */
+        $this->event = new Event();
+        $this->event->Title = 'Test Event Title';
+        $this->event->Details = 'This is detail about the test event title';
+        $this->event->startDateTime = $this->now;
+        $this->event->save();
+        $e = Event::get()->byID($this->event->ID);
+        error_log(print_r($e, 1));
+
+    }
+
+    public function test_default_embargo_date()
+    {
+
+    }
+}
